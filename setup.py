5c226d02b7e45d57448ad921575026099c915c8c
Remi Cattiau <remi@cattiau.com>
2016-05-09 20:29:52 -0700

    NXPY-4: Fix the version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'nuxeo-python-client',
   packages = ['nuxeo'],
-  version = '1.0',
+  version = '1.0.0',
   description = 'Nuxeo REST API Python client',
   author = 'Antoine Taillefer / Remi Cattiau',
   author_email = 'ataillefer@nuxeo.com rcattiau@nuxeo.com',
   url = 'https://github.com/nuxeo/nuxeo-python-client',
-  download_url = 'https://github.com/nuxeo/nuxeo-python-client/tarball/0.1',
+  download_url = 'https://github.com/nuxeo/nuxeo-python-client/tarball/1.0.0',
   keywords = ['nuxeo', 'ecm'],
   classifiers = [],
 )
