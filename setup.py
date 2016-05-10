d1476b8515cc5b7af22f847c29992f05cbcaa4c2
Remi Cattiau <remi@cattiau.com>
2016-05-09 20:34:32 -0700

    NXPY-4: Update version to next fix version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'nuxeo-python-client',
   packages = ['nuxeo'],
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'Nuxeo REST API Python client',
   author = 'Antoine Taillefer / Remi Cattiau',
   author_email = 'ataillefer@nuxeo.com rcattiau@nuxeo.com',
   url = 'https://github.com/nuxeo/nuxeo-python-client',
-  download_url = 'https://github.com/nuxeo/nuxeo-python-client/tarball/1.0.0',
+  download_url = 'https://github.com/nuxeo/nuxeo-python-client/tarball/1.0.1',
   keywords = ['nuxeo', 'ecm'],
   classifiers = [],
 )
