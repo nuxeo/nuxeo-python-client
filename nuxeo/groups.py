d340d808e35ab032b4a5eed6a72e44bffa0e332a
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-10-09 15:52:52 +0200

    NXPY-24: Cleanup groups

--- nuxeo/groups.py
+++ nuxeo/groups.py
@@ -1,37 +1,32 @@
 # coding: utf-8
+from __future__ import unicode_literals
+
 from .common import NuxeoObject, NuxeoService
 
 
 class Group(NuxeoObject):
-    """
-    Represent a Group on the server
-    """
+    """ Represent a Group on the server. """
+
     entity_type = 'group'
+
     def __init__(self, obj=None, service=None):
         super(Group, self).__init__(obj, service)
         self._entity_type = 'group'
         self.groupname = obj['groupname']
         self.grouplabel = obj['grouplabel']
-        if 'memberGroups' in obj:
-            self.memberGroups = obj['memberGroups']
-        else:
-            self.memberGroups = []
-        if 'memberUsers' in obj:
-            self.memberUsers = obj['memberUsers']
-        else:
-            self.memberUsers = []
+        self.memberGroups = obj.get('memberGroups', [])
+        self.memberUsers = obj.get('memberUsers', [])
 
     def get_id(self):
         return self.groupname
 
 
 class Groups(NuxeoService):
-    """
-    Groups management
-    """
+    """ Groups management. """
+
     def __init__(self, nuxeo):
         super(Groups, self).__init__(nuxeo, 'group', Group)
-        self._query = "?fetch.group=memberUsers&fetch.group=memberGroups"
+        self._query = '?fetch.group=memberUsers&fetch.group=memberGroups'
 
     def _get_args(self, obj):
         args = {'entity-type': self._object_class.entity_type}
@@ -41,36 +36,39 @@ class Groups(NuxeoService):
             args['memberUsers'] = obj.memberUsers
             args['memberGroups'] = obj.memberGroups
         elif isinstance(obj, dict):
-            for key in obj:
-                args[key] = obj[key]
+            args.update(obj)
         else:
-            raise Exception("Need a dictionary of properties or a " + self._object_class + " object")
+            err = 'Need a dictionary of properties or a {} object'
+            raise ValueError(err.format(self._object_class))
         return args
 
-    def get(self, id):
+    def get(self, uid):
         """
-        Get a group
+        Get a group.
 
-        :param id: Group name
+        :param uid: Group name
         :return:
         """
-        return self._nuxeo.request(self._path + '/' + id + self._query)
+        return self._nuxeo.request(self._path + '/' + uid + self._query)
 
     def update(self, obj):
         """
-        Update the group
+        Update the group.
 
         :param obj: Group object
         """
         args = self._get_args(obj)
-        self._nuxeo.request(self._path + '/' + obj.get_id() + self._query, body=args, method='PUT')
+        self._nuxeo.request(self._path + '/' + obj.get_id() + self._query,
+                            body=args, method='PUT')
 
     def create(self, obj):
         """
-        Create a new group
+        Create a new group.
 
         :param obj:
         :return: Group created
         """
         args = self._get_args(obj)
-        return self._object_class(self._nuxeo.request(self._path + self._query, method='POST', body=args), self)
+        req = self._nuxeo.request(
+            self._path + self._query, method='POST', body=args)
+        return self._object_class(req, self)
