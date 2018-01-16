4d82c0d6fa0bd6b2a3dbf8f720aa2dff1d3136ee
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-01-16 11:53:48 +0100

    NXPY-40: Format constants.py

--- nuxeo/constants.py
+++ nuxeo/constants.py
@@ -1,11 +1,25 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-CHUNK_SIZE = 8192  # 8 Kio - Chunk size to download files
-UPLOAD_CHUNK_SIZE = 256 * 1024  # 256 Kio - Size of chunks for the upload
-CHUNK_LIMIT = 10 * 1024 * 1024  # 10 Mio - Maximum file size before enforcing chunk upload
-MAX_RETRY = 3  # Retries for each upload/chunk upload before abandoning
+# Chunk size to download files
+CHUNK_SIZE = 8192  # 8 Kio
 
+# Size of chunks for the upload
+UPLOAD_CHUNK_SIZE = 256 * 1024  # 256 Kio
+
+# Maximum file size before enforcing chunk upload
+CHUNK_LIMIT = 10 * 1024 * 1024  # 10 Mio
+
+# Retries for each upload/chunk upload before abandoning
+MAX_RETRY = 3
+
+# API paths
 DEFAULT_URL = 'http://localhost:8080/nuxeo/'
 DEFAULT_API_PATH = 'api/v1'
+
+"""
+Default value for the:
+    - 'X-Application-Name' HTTP header
+    - 'applicationName' URL parameter
+"""
 DEFAULT_APP_NAME = 'Python client'
