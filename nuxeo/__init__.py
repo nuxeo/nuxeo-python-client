fd7eed701a5bff08872c0d8e830c3db283767caa
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-09-04 12:16:00 +0200

    NXPY-70: Release 2.0.3

--- nuxeo/__init__.py
+++ nuxeo/__init__.py
@@ -18,7 +18,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Nuxeo'
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
