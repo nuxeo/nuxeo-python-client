2b9ee0ae1e5d8e244755a1a73cd975a85868a519
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-01-31 15:57:52 +0100

    Bump the version to 2.4.4

--- nuxeo/__init__.py
+++ nuxeo/__init__.py
@@ -18,7 +18,7 @@ from __future__ import unicode_literals
 
 
 __author__ = "Nuxeo"
-__version__ = "2.4.3"
+__version__ = "2.4.4"
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
