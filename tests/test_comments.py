7ed102877aea1cafb0504f40b70ef2644cad0624
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-05-26 18:19:55 +0200

    NXPY-168: Rework Comments handling to work on all supported Nuxeo versions

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -22,15 +22,16 @@ def test_crud(server):
         assert not doc.comments()
 
         # Create a comment for that document
-        new_comment = Comment(parentId=doc.uid, text="This is my comment")
-        comment = server.comments.create(new_comment)
+        comment = server.comments.create(doc.uid, "This is my comment")
+        assert isinstance(comment, Comment)
 
         # Check we can retrieve the comment with its ID
-        assert server.comments.get(comment.uid)
+        assert server.comments.get(doc.uid, uid=comment.uid)
 
         # There is now 1 comment
         comments = doc.comments()
         assert len(comments) == 1
+        assert isinstance(comments[0], Comment)
         assert comments[0].text == "This is my comment"
 
         # Update that comment
@@ -38,8 +39,10 @@ def test_crud(server):
         comment.save()
 
         # Check the text has changed
-        assert doc.comments()[0].text == "Comment modified"
-        assert doc.comments()[0].modificationDate is not None
+        comments = doc.comments()
+        assert isinstance(comments[0], Comment)
+        assert comments[0].text == "Comment modified"
+        assert comments[0].modificationDate is not None
 
         # Delete the comment
         comment.delete()
@@ -57,22 +60,24 @@ def test_reply(server):
     doc = server.documents.create(document, parent_path=WORKSPACE_ROOT)
     try:
         # Create a comment for that document
-        new_comment = Comment(parentId=doc.uid, text="This is my comment")
-        comment = server.comments.create(new_comment)
+        comment = server.comments.create(doc.uid, "This is my comment")
         assert not comment.has_replies()
 
         # Add a 1st reply to that comment
         reply1 = comment.reply("This is my reply comment")
+        assert isinstance(reply1, Comment)
         assert comment.has_replies()
 
         # Check the comment has 1 reply (refetch it to ensure data is correct)
-        replies = server.comments.get(comment.uid)
+        replies = server.comments.get(doc.uid, uid=comment.uid)
+        assert isinstance(replies, Comment)
         assert replies.numberOfReplies == 1
         assert replies.numberOfReplies == comment.numberOfReplies
         assert replies.lastReplyDate == reply1.creationDate
 
         # Add a 2nd reply to that comment
         reply2 = comment.reply("This is another reply, yeah! ᕦ(ò_óˇ)ᕤ")
+        assert isinstance(reply2, Comment)
         assert comment.numberOfReplies == 2
         assert not reply2.has_replies()
 
@@ -80,15 +85,16 @@ def test_reply(server):
         last_reply = reply2.reply(
             "And a reply of the 2nd reply with \N{SNOWMAN}, boom!"
         )
+        assert isinstance(last_reply, Comment)
         assert reply2.has_replies()
 
         # Check the comment has 2 direct replies
-        replies = server.comments.get(comment.uid)
+        replies = server.comments.get(doc.uid, uid=comment.uid)
         assert replies.numberOfReplies == 2
         assert replies.lastReplyDate == reply2.creationDate
 
         # Check the 2nd reply has 1 reply
-        replies = server.comments.get(reply2.uid)
+        replies = server.comments.get(doc.uid, uid=reply2.uid)
         assert replies.numberOfReplies == 1
         assert replies.lastReplyDate == last_reply.creationDate
 
