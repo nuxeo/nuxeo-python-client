108b2cb00de600dfa0f40b6a6714290c67f5ca39
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-08-25 11:26:57 +0200

    NXPY-162: Bump the version number to 3.0.1

--- nuxeo/__init__.py
+++ nuxeo/__init__.py
@@ -19,7 +19,7 @@ from __future__ import unicode_literals
 
 
 __author__ = "Nuxeo"
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
