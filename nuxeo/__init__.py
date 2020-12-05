efbbe97e5de7afdc2164bc468f946b77d2b03b47
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-12-05 17:09:39 +0100

    NXPY-194: Release 4.0.0

--- nuxeo/__init__.py
+++ nuxeo/__init__.py
@@ -19,7 +19,7 @@ from __future__ import unicode_literals
 
 
 __author__ = "Nuxeo"
-__version__ = "3.1.2"
+__version__ = "4.0.0"
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
