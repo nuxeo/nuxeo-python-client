4a878b7fab50f98aac8185b646447d5ffffc89d0
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2021-05-07 15:42:25 +0200

    NXPY-221: Bump the version to 6.0.0

--- nuxeo/__init__.py
+++ nuxeo/__init__.py
@@ -19,7 +19,7 @@ from __future__ import unicode_literals
 
 
 __author__ = "Nuxeo"
-__version__ = "5.2.0"
+__version__ = "6.0.0"
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
