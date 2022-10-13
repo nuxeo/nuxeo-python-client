f3e41c0a5b01fe1904a83f6eafb0a0c67e18c367
gitofanindya <Anindya.Roy@hyland.com>
2022-10-13 11:50:43 +0530

    Fix tests cases on Python codecov

--- tests/constants.py
+++ tests/constants.py
@@ -2,4 +2,4 @@
 WORKSPACE_ROOT = "/default-domain/workspaces"
 WORKSPACE_NAME = "ws-python-tests"
 WORKSPACE_TEST = WORKSPACE_ROOT + "/" + WORKSPACE_NAME
-NUXEO_SERVER_URL = "http://localhost:8080/nuxeo/"  # "http://localhost:8080/nuxeo/" / "https://localhost:8443/nuxeo/"
+NUXEO_SERVER_URL = "http://localhost:8080/nuxeo"  # "http://localhost:8080/nuxeo" / "https://localhost:8443/nuxeo"
