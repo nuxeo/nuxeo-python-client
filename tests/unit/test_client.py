7e3bff15a992ac5e7f9f5ec8e9eb06a6b17bee40
gitofanindya <Anindya.Roy@hyland.com>
2024-02-27 11:58:19 +0530

    NXPY-253: Restore capability to use the client with a local HTTP server --Updated testcases

--- tests/unit/test_client.py
+++
@@ -1,9 +0,0 @@
-from ..constants import NUXEO_SERVER_URL
-from nuxeo.client import NuxeoClient
-
-
-def test_request():
-    client = NuxeoClient()
-    assert client.is_reachable()
-    client.ssl_verify_needed = None
-    assert client.request("GET", "", verify=False)
